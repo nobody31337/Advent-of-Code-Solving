ea4e7c97e46c27490fd6c0bda709f9dbf9c05875
Young-woong Lazarus Sohn <syw09191319@gmail.com>
2023-08-02 09:44:05 +0900

    Update solve.py

--- events/2016/day-20/solve.py
+++ events/2016/day-20/solve.py
@@ -40,21 +40,21 @@ for black_ in blacklist:
     
     if not isLowBlacked and low >= SMST:
         if low in keyval:
-            if keyval[low] == 'low':
+            if keyval[low] == 'left':
                 keyval[low] = 'both'
         else:
-            keyval[low] = 'high'
+            keyval[low] = 'right'
     if not isHighBlacked and high <= BGST:
         if high in keyval:
-            if keyval[high] == 'high':
+            if keyval[high] == 'right':
                 keyval[high] = 'both'
         else:
-            keyval[high] = 'low'
+            keyval[high] = 'left'
 
 keyval = sorted(keyval.items())
 
 for i in range(len(keyval)):
-    if i + 1 < len(keyval) and keyval[i][1] == 'low' and keyval[i + 1][1] == 'high':
+    if i + 1 < len(keyval) and keyval[i][1] == 'left' and keyval[i + 1][1] == 'right':
         whitelist.append((keyval[i][0], keyval[i + 1][0]))
     elif keyval[i][1] == 'both':
         whitelist.append((keyval[i][0], keyval[i][0]))
@@ -64,3 +64,19 @@ print('The answer:', whitelist[0][0])
 
 print('\nPart Two: How many IPs are allowed by the blacklist?')
 print('The answer:', sum(j - i + 1 for i, j in whitelist))
+
+'''
+def int_to_ip(ip: int):
+    ip1 = ip >> 24
+    ip2 = (ip >> 16) % 256
+    ip3 = (ip >> 8) % 256
+    ip4 = ip % 256
+    
+    return f'{ip1}.{ip2}.{ip3}.{ip4}'
+
+
+print('\n\nI\'m just gonna print all the IPs in the whitelist for fun.\n')
+for whited_range in whitelist:
+    for ip in range(whited_range[0], whited_range[1] + 1):
+        print(int_to_ip(ip))
+'''
