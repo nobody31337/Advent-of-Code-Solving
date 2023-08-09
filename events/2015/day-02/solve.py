ac91f3c522e3a9b4224d37311ba413cf8f38ddc9
Lazarus <syw09191319@gmail.com>
2023-08-09 11:48:00 +0900

    Update events/2015/day-02/solve.py

--- events/2015/day-02/solve.py
+++ events/2015/day-02/solve.py
@@ -13,3 +13,13 @@ response = requests.get(url, cookies=cookies)
 if response.status_code != 200:
     print('wrong cookies')
     exit(0)
+
+sizes = response.text
+
+total = 0
+
+for size in sizes.split():
+    dim = list(map(int,size.split('x')))
+    total += dim[0] * dim[1] + dim[1] * dim[2] + dim[0] * dim[2]
+
+print(total * 2)
