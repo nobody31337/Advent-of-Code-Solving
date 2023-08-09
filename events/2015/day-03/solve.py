f55868ad84fe7b4c1682470940263b91ba97121f
Lazarus <syw09191319@gmail.com>
2023-08-09 12:12:52 +0900

    Update events/2015/day-03/solve.py

--- events/2015/day-03/solve.py
+++ events/2015/day-03/solve.py
@@ -13,3 +13,23 @@ response = requests.get(url, cookies=cookies)
 if response.status_code != 200:
     print('wrong cookies')
     exit(0)
+
+directions = response.text
+
+cur = [0, 0]
+path = [cur]
+
+for d in directions:
+    match d:
+        case '>':
+            cur[0] += 1
+        case '<':
+            cur[0] -= 1
+        case '^':
+            cur[1] += 1
+        case 'v':
+            cur[1] -= 1
+    if cur not in path:
+        path.append(cur)
+
+print(path)
