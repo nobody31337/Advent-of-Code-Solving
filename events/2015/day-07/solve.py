39f2af3a60883039955ffda2800e6498f04643db
Lazarus <syw09191319@gmail.com>
2023-08-09 15:07:42 +0900

    Update events/2015/day-07/solve.py

--- events/2015/day-07/solve.py
+++ events/2015/day-07/solve.py
@@ -19,45 +19,40 @@ circuit = response.text.split('\n')[:-1]
 memo = {}
 
 def get(x: str):
-    print(f'Getting {x}')
-    time.sleep(0.2)
     ret = 0
 
-    if x not in memo:
-        match len(x.split()):
-            case 1:
-                try:
-                    return int(x)
-                except:
-                    for gate in circuit:
-                        lop, rop = gate.split(' -> ')
-                        if rop == x:
-                            ret = get(lop)
+    try:
+        return int(x)
+    except:
+        if x not in memo:
+            for gate in circuit:
+                left, right = gate.split(' -> ')
+                if right == x:
+                    match len(left.split()):
+                        case 1:
+                            ret = get(left)
 
-            case 2:
-                ret = ~int(get(x.split()[1])) & 0xffff
+                        case 2:
+                            ret = ~int(get(left.split()[1])) & 0xffff
 
-            case 3:
-                lop, op, rop = x.split()
-                match op:
-                    case 'AND':
-                        ret = get(lop) & get(rop)
-                    case 'OR':
-                        ret = get(lop) | get(rop)
-                    case 'RSHIFT':
-                        ret = get(lop) >> get(rop)
-                    case 'LSHIFT':
-                        ret = get(lop) << get(rop)
-            
-            case _:
-                ret = None
-        
-        memo[x] = ret
-    else:
-        ret = memo[x]
+                        case 3:
+                            lop, op, rop = left.split()
+                            match op:
+                                case 'AND':
+                                    ret = get(lop) & get(rop)
+                                case 'OR':
+                                    ret = get(lop) | get(rop)
+                                case 'RSHIFT':
+                                    ret = get(lop) >> get(rop)
+                                case 'LSHIFT':
+                                    ret = get(lop) << get(rop)
+                        
+                        case _:
+                            ret = None
+            memo[x] = ret
+        else:
+            ret = memo[x]
     
-    print(f'{x} == {ret}')
-
-    return ret
+        return ret
 
 print(get('a'))
