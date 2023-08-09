c4a5298c18cbea11aaf1cd7b71d55e97903698a1
Lazarus <syw09191319@gmail.com>
2023-08-09 11:42:27 +0900

    Update events/2016/day-02/solve.py

---
+++ events/2016/day-02/solve.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+with open('data.json', 'r') as js:
+    data = json.load(js)
+
+url = 'https://adventofcode.com/2016/day/2/input'
+
+cookies = data['cookies']
+
+response = requests.get(url, cookies=cookies)
+
+if response.status_code != 200:
+    print('wrong cookies')
+    exit(0)
