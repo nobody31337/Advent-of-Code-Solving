bc8a5c9593683da838b3b1a480a4c51cbf39cef0
Lazarus <syw09191319@gmail.com>
2023-08-09 14:46:28 +0900

    Update events/2015/day-07/solve.py

--- events/2015/day-07/solve.py
+++ events/2015/day-07/solve.py
@@ -14,9 +14,21 @@ if response.status_code != 200:
     print('wrong cookies')
     exit(0)
 
-circuit = response.text.split()[:-1]
+circuit = response.text.split('\n')[:-1]
 
 def get(x):
     try:
         return int(x)
     except:
+        match len(x.split()):
+            case 1:
+                for gate in circuit:
+                    lop, rop = gate.split(' -> ')
+                    if rop == x:
+                        return get(lop)
+            case 2:
+                return ~int(get(x.split()[1])) & 0xffff
+            case 3:
+                pass
+
+print(get('NOT 123'))
