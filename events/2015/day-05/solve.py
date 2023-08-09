9e513469f941136076d537f0c291794a49c3e2eb
Lazarus <syw09191319@gmail.com>
2023-08-09 13:18:53 +0900

    Update events/2015/day-05/solve.py

--- events/2015/day-05/solve.py
+++ events/2015/day-05/solve.py
@@ -20,15 +20,17 @@ words = response.text.split()
 vowels = 'aeiou'
 
 nice = 0
-naughty = 0
 
 for word in words:
     if sum(word.count(v) for v in vowels) > 2 and re.search(r'([a-z])\1', word) and not ('ab' in word or 'cd' in word or 'pq' in word or 'xy' in word):
-        print('Noice', word)
         nice += 1
-    else:
-        print('Bathds', word)
-        naughty += 1
 
-print('Nice words:', nice)
-print('Naughty words:', naughty)
+print('Nice words part 1:', nice)
+
+nice = 0
+
+for word in words:
+    if re.search(r'([a-z]{2})[a-z]?\1', word) and re.search(r'([a-z])[a-z]\1', word):
+        nice += 1
+
+print('Nice words part 2:', nice)
