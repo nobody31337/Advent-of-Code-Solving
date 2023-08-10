445627ab4d1ba1f499cc68cfa245dd6f04f4640e
Lazarus <syw09191319@gmail.com>
2023-08-10 11:21:36 +0900

    Update events/2015/day-09/solve.py

--- events/2015/day-09/solve.py
+++ events/2015/day-09/solve.py
@@ -34,31 +34,46 @@ visited = []
 partone = 0
 parttwo = 0
 
+def search_min_old(loc = None, next_loc = None):
+    if loc is None:
+        return min(search_min_old(x) for x in graph)
+
+    if next_loc is None:
+        visited.append(loc)
+        ret = min(search_min_old(loc, x) for x in graph[loc] if x not in visited)
+        visited.remove(loc)
+        return ret
+    
+    return graph[loc][next_loc] + (search_min_old(next_loc) if len(set(graph[next_loc])-set(visited)) else 0)
+
+
+def search_max_old(loc = None, next_loc = None):
+    if loc is None:
+        return max(search_max_old(x) for x in graph)
+
+    if next_loc is None:
+        visited.append(loc)
+        ret = max(search_max_old(loc, x) for x in graph[loc] if x not in visited)
+        visited.remove(loc)
+        return ret
+    
+    return graph[loc][next_loc] + (search_max_old(next_loc) if len(set(graph[next_loc])-set(visited)) else 0)
+
+
 def search_min(loc = None, next_loc = None):
     if loc is None:
         return min(search_min(x) for x in graph)
 
     if next_loc is None:
         visited.append(loc)
-        ret = min(search_min(loc, x) for x in graph[loc] if x not in visited)
+        ret = min((x, graph[loc][x]) for x in graph[loc] if x not in visited)
         visited.remove(loc)
         return ret
     
-    return graph[loc][next_loc] + (search_min(next_loc) if len(set(graph[next_loc])-set(visited)) else 0)
+    return graph[loc][next_loc] + (search_min_old(next_loc) if len(set(graph[next_loc])-set(visited)) else 0)
 
-
-def search_max(loc = None, next_loc = None):
-    if loc is None:
-        return max(search_max(x) for x in graph)
-
-    if next_loc is None:
-        visited.append(loc)
-        ret = max(search_max(loc, x) for x in graph[loc] if x not in visited)
-        visited.remove(loc)
-        return ret
-    
-    return graph[loc][next_loc] + (search_max(next_loc) if len(set(graph[next_loc])-set(visited)) else 0)
-
+print((x, graph['Arbre'][x]) for x in graph['Arbre'] if x not in visited)
+print(min((x, graph['Arbre'][x]) for x in graph['Arbre'] if x not in visited))
 
 partone = search_min()
 parttwo = search_max()
