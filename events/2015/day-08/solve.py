50a23c3a88d3dcfe8440fc6cee1ab50dca498dfd
Lazarus <syw09191319@gmail.com>
2023-08-10 09:31:58 +0900

    Update events/2015/day-08/solve.py

--- events/2015/day-08/solve.py
+++ events/2015/day-08/solve.py
@@ -23,8 +23,7 @@ for string in strings:
     lit_len = len(string)
     esc_len = len(string[1:-1].encode().decode('unicode_escape'))
     partone += lit_len - esc_len
-    print('"' + string.replace('\\', '\\\\').replace('"', '\\"') + '"')
-    parttwo += len(string.replace('"', '\\"').replace('\\', '\\\\')) - lit_len
+    parttwo += len('"' + string.replace('\\', '\\\\').replace('"', '\\"') + '"') - lit_len
 
 print(partone)
 print(parttwo)
