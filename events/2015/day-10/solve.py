984569f432b27fa15593bcfc832f174682c4ae39
Lazarus <syw09191319@gmail.com>
2023-08-10 12:17:56 +0900

    Update events/2015/day-10/solve.py

--- events/2015/day-10/solve.py
+++ events/2015/day-10/solve.py
@@ -17,21 +17,22 @@ if response.status_code != 200:
 digits = response.text.strip()
 
 def look_and_say(inp, time: int = 1):
-    if time:
-        if not isinstance(inp, str):
-            inp = str(inp)
-        
-        digit = inp[0]
-        ret = ''
-        count = 0
-        for n in inp:
-            if digit != n:
-                ret += str(count) + digit
-                digit = n
-                count = 0
-            count += 1
-        ret += str(count) + digit
-        print(ret)
-        look_and_say(ret, time-1)
+    if not isinstance(inp, str):
+        inp = str(inp)
+    
+    if time <= 0:
+         return inp
+    
+    digit = inp[0]
+    ret = ''
+    count = 0
+    for n in inp:
+        if digit != n:
+            ret += str(count) + digit
+            digit = n
+            count = 0
+        count += 1
+    ret += str(count) + digit
+    return look_and_say(ret, time-1)
 
-look_and_say(digits, 40)
+print(len(look_and_say(digits, 40)))
