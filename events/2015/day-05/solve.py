e25ad8b02d2c941ebd07a0959667fc5ae2f72ea2
Lazarus <syw09191319@gmail.com>
2023-08-10 12:50:54 +0900

    Update events/2015/day-05/solve.py

--- events/2015/day-05/solve.py
+++ events/2015/day-05/solve.py
@@ -19,18 +19,14 @@ words = response.text.split()
 
 vowels = 'aeiou'
 
-nice = 0
+partone = 0
+parttwo = 0
 
 for word in words:
     if sum(word.count(v) for v in vowels) > 2 and re.search(r'([a-z])\1', word) and not ('ab' in word or 'cd' in word or 'pq' in word or 'xy' in word):
-        nice += 1
-
-print('Nice words part 1:', nice)
-
-nice = 0
-
-for word in words:
+        partone += 1
     if re.search(r'([a-z]{2})[a-z]*\1', word) and re.search(r'([a-z])[a-z]\1', word):
-        nice += 1
+        parttwo += 1
 
-print('Nice words part 2:', nice)
+print('Nice words part 1:', partone)
+print('Nice words part 2:', parttwo)
