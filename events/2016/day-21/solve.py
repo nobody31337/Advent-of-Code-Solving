7e25348e9f64ec82d11b37d67712a64327615297
Lazarus <syw09191319@gmail.com>
2023-08-11 10:48:03 +0900

    Update events/2016/day-21/solve.py

--- events/2016/day-21/solve.py
+++ events/2016/day-21/solve.py
@@ -44,7 +44,7 @@ def scramble(password: str, steps):
                     password = password[-int(x):] + password[:-int(x)]
             case ['move', 'position', x, _, _, y]: # move position X to position Y
                 password.insert(int(y), password.pop(int(x)))
-    
+
     return ''.join(password)
 
 
@@ -65,7 +65,7 @@ def reverse_scramble(password: str, steps):
                 password = password[:x] + password[x:y][::-1] + password[y:]
             case ['rotate', 'based', _, _, _, _, x]: # rotate based on position of letter X
                 idx = password.index(x)
-                shift = (idx - (1 if idx % 2 else 2)) // 2
+                shift = 
                 password = password[shift:] + password[:shift]
             case ['rotate', direction, x, _]: # rotate left/right X steps
                 if direction == 'left':
