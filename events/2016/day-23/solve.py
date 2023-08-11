2c97640d5674860d0c936417553aadef35911c3d
Lazarus <syw09191319@gmail.com>
2023-08-11 17:24:48 +0900

    Update events/2016/day-23/solve.py

--- events/2016/day-23/solve.py
+++ events/2016/day-23/solve.py
@@ -54,6 +54,12 @@ def run(regs: dict[str, int], steps: list[str]):
 partone = dict(a=7, b=0, c=0, d=0)
 parttwo = dict(a=144, b=0, c=0, d=0)
 
+i = 0
+while True:
+    tmp = dict(a=i, b=0, c=0, d=0)
+    run(tmp, assembunny)
+    print()
+
 print('Part One: What value should be sent to the safe?')
 run(partone, assembunny)
 print('The answer:', partone['a'], partone)
