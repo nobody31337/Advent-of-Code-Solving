7f08b85bbf0c4f4f1fb02e02d08ae48a495d4e32
Lazarus <syw09191319@gmail.com>
2023-08-11 09:13:32 +0900

    Update events/2016/day-21/solve.py

--- events/2016/day-21/solve.py
+++ events/2016/day-21/solve.py
@@ -15,26 +15,28 @@ if response.status_code != 200:
     exit(0)
 
 word = list('abcdefgh')
-
-for step in response.text.splitlines():
-    match step.split():
-        case ['swap', 'position', x, _, _, y]: # swap position X with position Y
-            x = int(x)
-            y = int(y)
-            word[x], word[y] = word[y], word[x]
-        case ['swap', 'letter', x, _, _, y]: # swap letter X with letter Y
-            for i in range(len(word)):
-                word[i] = y if word[i] == x else x if word[i] == y else word[i]
-        case ['reverse', _, x, _, y]: # reverse positions X through Y
-            x = int(x)
-            y = int(y) + 1
-            word = word[0:x] + word[x:y:-1] + word[y:]
-        case ['rotate', 'based', _, _, _, _, x]: # rotate based on position of letter X
-            pass
-        case ['rotate', direction, x, _]: # rotate left/right X steps
-            pass
-        case ['move', 'position', x, _, _, y]: # move position X to position Y
-            pass
-
+try:
+    for step in response.text.splitlines():
+        match step.split():
+            case ['swap', 'position', x, _, _, y]: # swap position X with position Y
+                x = int(x)
+                y = int(y)
+                word[x], word[y] = word[y], word[x]
+            case ['swap', 'letter', x, _, _, y]: # swap letter X with letter Y
+                for i in range(len(word)):
+                    word[i] = y if word[i] == x else x if word[i] == y else word[i]
+            case ['reverse', _, x, _, y]: # reverse positions X through Y
+                x = int(x)
+                y = int(y) + 1
+                word = word[0:x] + word[x:y:-1] + word[y:]
+            case ['rotate', 'based', _, _, _, _, x]: # rotate based on position of letter X
+                pass
+            case ['rotate', direction, x, _]: # rotate left/right X steps
+                pass
+            case ['move', 'position', x, _, _, y]: # move position X to position Y
+                pass
+except:
+    print(word)
+    raise
 print(''.join(word))
 print(word[0:0])
