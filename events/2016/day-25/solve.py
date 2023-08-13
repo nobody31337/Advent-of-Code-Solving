ac7ee6049c7cf1a7d4971355f963771ea48a1e55
Lazarus <syw09191319@gmail.com>
2023-08-14 00:48:08 +0900

    Update events/2016/day-25/solve.py

--- events/2016/day-25/solve.py
+++ events/2016/day-25/solve.py
@@ -16,8 +16,6 @@ if response.status_code != 200:
 
 assembunny = response.text.splitlines()
 
-class MatchBreak(Exception): pass
-
 sglarg = ('inc', 'dec', 'tgl', 'out')
 dblarg = ('jnz', 'cpy')
 
@@ -29,55 +27,53 @@ def run(regs: dict[str, int], steps: list[str]):
 
     while 0 <= i < len(steps):
         offset = 1
-        try:
-            match steps[i]:
-                case ['cpy', x, y]:
-                    if y in regs:
-                        match steps[i+1:i+6]:
-                            case [['inc', a], ['dec', y1], ['jnz', y2, '-2'], ['dec', b], ['jnz', b2, '-5']]:
-                                if y == y1 == y2 and b == b2:
-                                    regs[y] = 0
-                                    regs[a] += regs[b] * (regs[x] if x in regs else int(x))
-                                    regs[b] = 0
-                                    offset = 6
-                                    raise MatchBreak
-                        regs[y] = regs[x] if x in regs else int(x)
-                case ['inc', x]:
-                    if x in regs:
-                        match steps[i+1:i+3]:
-                            case [['dec', a], ['jnz', a1, '-2']]:
-                                if a == a1:
-                                    regs[x] += regs[a]
-                                    regs[a] = 0
-                                    offset = 3
-                                    raise MatchBreak
-                        regs[x] += 1
-                case ['dec', x]:
-                    if x in regs:
-                        regs[x] -= 1
-                case ['jnz', x, y]:
-                    x = regs[x] if x in regs else int(x)
-                    y = regs[y] if y in regs else int(y)
-                    offset = y if x else 1
-                case ['tgl', x]:
-                    x = regs[x] if x in regs else int(x)
-                    if i + x >= len(steps) or i + x < 0:
-                        raise MatchBreak()
-                    if steps[i+x][0] in sglarg:
-                        steps[i+x][0] = 'dec' if steps[i+x][0] == 'inc' else 'inc'
-                    elif steps[i+x][0] in dblarg:
-                        steps[i+x][0] = 'cpy' if steps[i+x][0] == 'jnz' else 'jnz'
-                case ['out', x]:
-                    output.append(regs[x])
+        match steps[i]:
+            case ['cpy', x, y]:
+                if y in regs:
+                    match steps[i+1:i+6]:
+                        case [['inc', a], ['dec', y1], ['jnz', y2, '-2'], ['dec', b], ['jnz', b2, '-5']]:
+                            if y == y1 == y2 and b == b2:
+                                regs[y] = 0
+                                regs[a] += regs[b] * (regs[x] if x in regs else int(x))
+                                regs[b] = 0
+                                i += 6
+                                continue
+                    regs[y] = regs[x] if x in regs else int(x)
+            case ['inc', x]:
+                if x in regs:
+                    match steps[i+1:i+3]:
+                        case [['dec', a], ['jnz', a1, '-2']]:
+                            if a == a1:
+                                regs[x] += regs[a]
+                                regs[a] = 0
+                                i += 3
+                                continue
+                    regs[x] += 1
+            case ['dec', x]:
+                if x in regs:
+                    regs[x] -= 1
+            case ['jnz', x, y]:
+                x = regs[x] if x in regs else int(x)
+                y = regs[y] if y in regs else int(y)
+                offset = y if x else 1
+            case ['tgl', x]:
+                x = regs[x] if x in regs else int(x)
+                if i + x >= len(steps) or i + x < 0:
+                    i += 1
+                    continue
+                if steps[i+x][0] in sglarg:
+                    steps[i+x][0] = 'dec' if steps[i+x][0] == 'inc' else 'inc'
+                elif steps[i+x][0] in dblarg:
+                    steps[i+x][0] = 'cpy' if steps[i+x][0] == 'jnz' else 'jnz'
+            case ['out', x]:
+                output.append(regs[x])
 
-                    if len(output) > 1 and output[-1] == output[-2]:
-                            break
+                if len(output) > 1 and output[-1] == output[-2]:
+                        break
 
-                    if regs in trace:
-                        return True
-                    trace.append(regs.copy())
-        except MatchBreak:
-            pass
+                if regs in trace:
+                    return True
+                trace.append(regs.copy())
         i += offset
     
     return False
