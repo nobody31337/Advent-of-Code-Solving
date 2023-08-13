f286725f27e5a7be461e839f835902a1e3d77b9e
Lazarus <syw09191319@gmail.com>
2023-08-14 00:59:36 +0900

    Update events/2016/day-12/solve.py

--- events/2016/day-12/solve.py
+++ events/2016/day-12/solve.py
@@ -25,28 +25,31 @@ def run(regs: dict[str, int], steps: list[str]):
     i = 0
     while 0 <= i < len(steps):
         offset = 1
+
+        # Long codes short
+        match steps[i:i+3]:
+            case [['inc', x], ['dec', a], ['jnz', a1, '-2']]:
+                if a == a1:
+                    regs[x] += regs[a]
+                    regs[a] = 0
+                    i += 3
+                    continue
+
+        match steps[i:i+6]:
+            case [['cpy', x, y], ['inc', a], ['dec', y1], ['jnz', y2, '-2'], ['dec', b], ['jnz', b2, '-5']]:
+                if y == y1 == y2 and b == b2:
+                    regs[y] = 0
+                    regs[a] += regs[b] * (regs[x] if x in regs else int(x))
+                    regs[b] = 0
+                    i += 6
+                    continue
         
         match steps[i]:
             case ['cpy', x, y]:
                 if y in regs:
-                    match steps[i+1:i+6]:
-                        case [['inc', a], ['dec', y1], ['jnz', y2, '-2'], ['dec', b], ['jnz', b2, '-5']]:
-                            if y == y1 == y2 and b == b2:
-                                regs[y] = 0
-                                regs[a] += regs[b] * (regs[x] if x in regs else int(x))
-                                regs[b] = 0
-                                i += 6
-                                continue
                     regs[y] = regs[x] if x in regs else int(x)
             case ['inc', x]:
                 if x in regs:
-                    match steps[i+1:i+3]:
-                        case [['dec', a], ['jnz', a1, '-2']]:
-                            if a == a1:
-                                regs[x] += regs[a]
-                                regs[a] = 0
-                                i += 3
-                                continue
                     regs[x] += 1
             case ['dec', x]:
                 if x in regs:
@@ -55,15 +58,6 @@ def run(regs: dict[str, int], steps: list[str]):
                 x = regs[x] if x in regs else int(x)
                 y = regs[y] if y in regs else int(y)
                 offset = y if x else 1
-            case ['tgl', x]:
-                x = regs[x] if x in regs else int(x)
-                if i + x >= len(steps) or i + x < 0:
-                    i += 1
-                    continue
-                if steps[i+x][0] in sglarg:
-                    steps[i+x][0] = 'dec' if steps[i+x][0] == 'inc' else 'inc'
-                elif steps[i+x][0] in dblarg:
-                    steps[i+x][0] = 'cpy' if steps[i+x][0] == 'jnz' else 'jnz'
 
         i += offset
 
