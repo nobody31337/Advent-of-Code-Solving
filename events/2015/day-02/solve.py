d9b56aa1ab4be4c2af23ce4a257ed478f8a04dfe
Lazarus <syw09191319@gmail.com>
2023-08-14 02:43:23 +0900

    Update events/2015/day-02/solve.py

--- events/2015/day-02/solve.py
+++ events/2015/day-02/solve.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from timeit import default_timer as timer
 
 with open('data.json', 'r') as js:
     data = json.load(js)
@@ -21,6 +22,8 @@ parttwo = 0
 
 tmp = []
 
+start = timer()
+
 for size in sizes:
     dim = list(map(int, size.split('x')))
     partone += 2 * (dim[0] * dim[1] + dim[1] * dim[2] + dim[0] * dim[2])
@@ -29,8 +32,12 @@ for size in sizes:
     partone += tmp[0] * tmp[1]
     parttwo += 2 * (tmp[0] + tmp[1]) + dim[0] * dim[1] * dim[2]
 
+end = timer() - start
+
 print('Part One: How many total square feet of wrapping paper should they order?')
 print('The answer:', partone)
 
 print('\nPart Two: How many total feet of ribbon should they order?')
 print('The answer:', parttwo)
+
+print(f'\nProcess time: {round(end*1000, 6)} ms')
