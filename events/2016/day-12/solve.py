fb82bd874d35fed80bf997d1f56a95a9d7cc0a0d
Lazarus <syw09191319@gmail.com>
2023-08-14 01:04:37 +0900

    Update events/2016/day-12/solve.py

--- events/2016/day-12/solve.py
+++ events/2016/day-12/solve.py
@@ -20,6 +20,7 @@ assembunny = response.text.splitlines()
 def run(regs: dict[str, int], steps: list[str]):
     steps = list(map(lambda line: line.split(), steps))
     i = 0
+    
     while 0 <= i < len(steps):
         offset = 1
 
