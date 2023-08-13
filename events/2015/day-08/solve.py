88317ca203b4e02a5454544f6e1ebabf94ffa010
Lazarus <syw09191319@gmail.com>
2023-08-14 02:31:20 +0900

    Update events/2015/day-08/solve.py

--- events/2015/day-08/solve.py
+++ events/2015/day-08/solve.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from timeit import default_timer as timer
 
 with open('data.json', 'r') as js:
     data = json.load(js)
@@ -19,6 +20,8 @@ strings = response.text.splitlines()
 partone = 0
 parttwo = 0
 
+start = timer()
+
 for string in strings:
     lit_len = len(string)
     esc_len = len(eval(string))
@@ -26,8 +29,12 @@ for string in strings:
     partone += lit_len - esc_len
     parttwo += len(string) + string.count('"') + string.count('\\') + 2 - lit_len
 
+end = timer() - start
+
 print('Part One: What is the given data length minus unescaped string data length?')
 print(partone)
 
 print('\nPart Two: What is the length of the escaped string of the given data minus given data length?')
 print(parttwo)
+
+print(f'\nProcess time: {round(end*1000, 6)} ms')
