7e505f852d557c44313776dae4ec80951ce7c537
Lazarus <syw09191319@gmail.com>
2023-08-14 00:51:43 +0900

    Update events/2016/day-23/solve.py

--- events/2016/day-23/solve.py
+++ events/2016/day-23/solve.py
@@ -79,5 +79,6 @@ print('The answer:', parttwo['a'])
 
 print('\nJust for fun:')
 
-for i in range(21):
-    print(f'{i:2}: {run(dict(a=i, b=0, c=0, d=0), assembunny)}')
+for i in range(1, 21):
+    print(f'{i:2}:', end=' ')
+    print(f'{run(dict(a=i, b=0, c=0, d=0), assembunny)}')
