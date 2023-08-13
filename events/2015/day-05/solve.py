6bf8ed74304152aca1d26d6fbc733f510c60e19f
Lazarus <syw09191319@gmail.com>
2023-08-14 02:46:20 +0900

    Update events/2015/day-05/solve.py

--- events/2015/day-05/solve.py
+++ events/2015/day-05/solve.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import re
+from timeit import default_timer as timer
 
 with open('data.json', 'r') as js:
     data = json.load(js)
@@ -17,6 +18,8 @@ if response.status_code != 200:
 
 words = response.text.split()
 
+start = timer()
+
 vowels = 'aeiou'
 
 partone = 0
@@ -28,5 +31,9 @@ for word in words:
     if re.search(r'([a-z]{2})[a-z]*\1', word) and re.search(r'([a-z])[a-z]\1', word):
         parttwo += 1
 
+end = timer() - start
+
 print('Nice words part 1:', partone)
 print('Nice words part 2:', parttwo)
+
+print(f'\nProcess time: {round(end*1000, 6)} ms')
