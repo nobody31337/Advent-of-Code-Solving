df94f5f7c0eb77e1091d18e66e24c3dd4a98affc
Lazarus <syw09191319@gmail.com>
2023-08-14 01:04:37 +0900

    Update events/2016/day-23/solve.py

--- events/2016/day-23/solve.py
+++ events/2016/day-23/solve.py
@@ -22,6 +22,7 @@ dblarg = ('jnz', 'cpy')
 def run(regs: dict[str, int], steps: list[str]):
     steps = list(map(lambda line: line.split(), steps))
     i = 0
+    
     while 0 <= i < len(steps):
         offset = 1
 
