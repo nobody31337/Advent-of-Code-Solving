c921bc38469155b9b19526648ece7c25d576ea18
Lazarus <syw09191319@gmail.com>
2023-08-14 00:26:12 +0900

    Update events/2016/day-25/solve.py

--- events/2016/day-25/solve.py
+++ events/2016/day-25/solve.py
@@ -68,6 +68,7 @@ def run(regs: dict[str, int], steps: list[str]):
                     elif steps[i+x][0] in dblarg:
                         steps[i+x][0] = 'cpy' if steps[i+x][0] == 'jnz' else 'jnz'
                 case ['out', x]:
+                    print(regs[x], end=' ')
                     # print(regs[x] if x in regs else x, regs, regs[x] ^ heartbeat)
 
                     if len(trace) > 0 and not (regs[x] ^ heartbeat):
