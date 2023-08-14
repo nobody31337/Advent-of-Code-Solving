031fbb0c57398580e358c5d822b9a62b321cd85a
Lazarus <syw09191319@gmail.com>
2023-08-14 11:50:39 +0900

    Update time_measure.py

--- time_measure.py
+++ time_measure.py
@@ -2,11 +2,19 @@ import sys
 import subprocess
 from timeit import default_timer as timer
 
-if len(sys.argv) > 1:
-    process = [sys.executable, sys.argv[1]]
+def get_time(time: float):
+    if time > 1:
+        return f'{round(time, 6)} seconds'
+    else:
+        return f'{round(time * 1000, 6)} ms'
 
-    start = timer()
-    subprocess.run(process)
-    done = timer() - start
-    
-    print(f'\nElapsed time: {round(done, 6)} seconds')
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        process = [sys.executable, sys.argv[1]]
+
+        start = timer()
+        subprocess.run(process)
+        done = timer() - start
+        
+        print(f'\nElapsed time: {round(done, 6)} seconds')
