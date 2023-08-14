5d99f1be21220721a99e30236bfded24f2420cc8
Lazarus <syw09191319@gmail.com>
2023-08-14 09:29:02 +0900

    Delete modification.py

--- modification.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for year in range(2015, 2023):
-    for day in range(1, 26):
-        path = f'./events/{year}/day-{day:02}'
-
-        # os.makedirs(path)
-
-        # with open(path + '/solve.py', 'a') as f: pass
