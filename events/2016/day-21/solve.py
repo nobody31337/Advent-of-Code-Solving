2fdc3e80d3fcb65dc9092f8a53aad6b8dc3ea3f7
Lazarus <syw09191319@gmail.com>
2023-08-14 10:12:09 +0900

    Update events/2016/day-21/solve.py

--- events/2016/day-21/solve.py
+++ events/2016/day-21/solve.py
@@ -72,7 +72,7 @@ def reverse_scramble(password: str, steps):
                 idx = password.index(x)
                 # shift = (idx + (idx > 3) + 1) % length
                 shift = next(i for i in range(length) if (i * 2 + (i > 3) + 1) % length == idx)
-                password = password[shift:] + password[:shift]
+                password = password[-shift:] + password[:-shift]
             case ['rotate', direction, x, _]: # rotate left/right X steps
                 if direction == 'left':
                     password = password[-int(x):] + password[:-int(x)]
